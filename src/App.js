934f78af7f0f7ffc573f9779da270d28cee3f66b
Jake Harris <harri676@purdue.edu>
2018-06-11 12:58:26 -0400

    Changes article title to a React component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './Header'
 import Ad from './Ad'
 import Article from './Article'
+import ArticleTitle from './ArticleTitle'
 
 class App extends Component {
   render() {
@@ -13,14 +14,7 @@ class App extends Component {
 
       <main className="expanded row">
         <div className="large-8 medium-12 columns article">
-          <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-          <div className="avatar">
-            <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-            <div className="author-info">
-              <p className="author-name">By Domri, son of Flug</p>
-              <p className="date">on 28 April, 3018 of the Third Age</p>
-            </div>
-          </div>
+          <ArticleTitle />
 
           <Article />
           
