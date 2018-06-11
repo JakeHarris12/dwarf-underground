1a5e8be97dd874112b6b8db9d24ec3c12429c317
Jake Harris <harri676@purdue.edu>
2018-06-11 13:40:35 -0400

    Changes the clickbait to React components and creates them using a map

--- src/App.js
+++ src/App.js
@@ -7,7 +7,43 @@ import Article from './Article'
 import ArticleTitle from './ArticleTitle'
 import Footer from './Footer'
 
+class ClickBait extends Component {
+  render(){
+    return(
+      <div>
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src={this.props.pic} alt={this.props.alts} />
+            <p>{this.props.title}</p>
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
 class App extends Component {
+
+  constructor() {
+    super()
+    
+    this.state = {
+      clickbait: [
+        {title: 'Single Orcs in Indianapolis',
+          pic: 'https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif',
+          alts: 'orc'},
+        {title: 'You won\'t believe what\'s under this mountain',
+          pic: 'https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg',
+          alts: 'mountain'},
+        {title: 'Mine 20% more gold with One Weird Trick',
+          pic: 'http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg',
+          alts: 'gold'},
+        {title: 'Surprise for Indiana Hobbits born before 1999',
+          pic: 'http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg',
+          alts: 'hobbit'},
+      ],
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -35,30 +71,9 @@ class App extends Component {
 
         <div className="small-12 columns other-articles">
           <h2>From around the Realm</h2>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
-              <p>Single Orcs in Indianapolis</p>
-            </a>
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
-              <p>You won't believe what's under this mountain</p>
-            </a>
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
-              <p>Mine 20% more gold with One Weird Trick</p>
-            </a>
-          </div>
-          <div className="small-6 medium-3 columns other-article">
-            <a href="#">
-              <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
-              <p>Surprise for Indiana Hobbits born before 1999</p>
-            </a>
-          </div>
+
+          {this.state.clickbait.map(clickbait => <ClickBait title={clickbait.title} pic={clickbait.pic} alts={clickbait.alts} />)}
+
         </div>
       </main>
 
